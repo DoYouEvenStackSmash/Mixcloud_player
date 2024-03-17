5f36ee8cdb485643f639d14e5d7ff41a87e93086
DoYouEvenStackSmash <42816626+DoYouEvenStackSmash@users.noreply.github.com>
2024-03-17 11:44:51 -0700

    add samples

--- custom.js
+++ custom.js
@@ -1,6 +1,11 @@
 // sample file, follow the patterns
 var CUSTOM_PLAYLISTS_TO_LOAD = [
 	{mixcloud_search_key:"miss%20monique", mixcloud_match_regex:/.*?miss\s*monique.*/i},
+        // sample for how to search by key then match by regex and reformat song titles
+	//{mixcloud_search_key:"rons%20podcast%20%23", mixcloud_match_regex:/RONS.*?Podcast.*?\d{3,4}/i, name_to_replace:/RONS:? Podcast #?(\w+)/gmi, name_replace_with:`RONS: Podcast #$1`},
 	
 ];
-var INDIVIDUAL_TRACKS_TO_LOAD = [];
+var INDIVIDUAL_TRACKS_TO_LOAD = [
+	// sample to load individual mixcloud tracks:
+	// {name:'my custom name', key:'/Hudson921/2000s-2010s-mix-2323/'}, // points to https://www.mixcloud.com/Hudson921/2000s-2010s-mix-2323/
+];
